7ed1e95877ac2a30e31f39e0b5dfda82d97b89e2
Manuel A. Fernandez <mf@foundational.co>
2014-12-28 10:59:09 -0300

    #Created main function, segregated primary and secondary DNS server querying.
    Signed-off-by: Manuel A. Fernandez <mf@foundational.co>

--- dns.ps1
+++ dns.ps1
@@ -7,12 +7,26 @@ param(
 $DNSArray = @($PrimaryDNSServer,$SecondaryDNSServer)
 $DNSHashedValues = @{DNSServer = ''; ResolvedIPAddress = ''}
 
-Write-Host "Usage: dns.ps1 -target [hostname] -primaryDNS [IP address] -secondaryDNS [IP Address]"
+function resolveDNS {
+		$DNSHashedValues.DNSServer = $element
+		$FormattedIPAddress = Resolve-DnsName -name $DNSHostname -Server $element -Type A | Select -Property IPAddress
+		$DNSHashedValues.ResolvedIPAddress = $FormattedIPAddress.IPAddress
+		$DNSHashedValues
+		Write-Host '----------'
+}
+
+Write-Host "Usage: dns-queryd.ps1 -target [hostname] -primaryDNS [IP address] -secondaryDNS [IP Address]"
 Write-Host "Domain name:" $DNSHostname
+
 foreach ($element in $DNSArray) {
-	$DNSHashedValues.DNSServer = $element
-	$FormattedIPAddress = Resolve-DnsName -name $DNSHostname -Server $element -Type A | Select -Property IPAddress
-	$DNSHashedValues.ResolvedIPAddress = $FormattedIPAddress.IPAddress
-    $DNSHashedValues
-    Write-Host '----------'
+	if ($element -eq $DNSArray[0]) {
+		Write-Host "Resolving host name using primary DNS server" $PrimaryDNSServer
+		resolveDNS
+	} elseif ($element -eq $DNSArray[1]) {
+		Write-Host "Resolving host name using secondary DNS server" $PrimaryDNSServer
+		resolveDNS
+	} else {	
+		Write-Host "Error: use only primary or secondary DNS servers."
+		break
+	}
 }
